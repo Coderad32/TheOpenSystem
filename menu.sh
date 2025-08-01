28fddeed5c7eb737a36f15ed7a58457bd5f4cba4
Cody <162437900+Coderad32@users.noreply.github.com>
2025-08-01 11:57:25 -0600

    Create menu.sh
    add menu shell code

---
+++ menu.sh
@@ -0,0 +1,44 @@
+```sh
+import system
+
+use system
+
+#!/bin/bash
+
+while true; do
+    echo "Main Menu"
+    echo "0. Menu (Main)"
+    echo "1. option goto"
+    echo "2. option todo"
+    echo "3. option route"
+    echo "4. back file"
+    echo "  exit"
+    echo -n "Enter your choice (0-4 or exit): "
+    read choice
+
+    case $choice in
+        0)
+            echo "You selected: Menu (Main)"
+            ;;
+        1)
+            echo "You selected: option goto"
+            ;;
+        2)
+            echo "You selected: option todo"
+            ;;
+        3)
+            echo "You selected: option route"
+            ;;
+        4)
+            echo "You selected: back file"
+            ;;
+        exit)
+            echo "Exiting. Goodbye!"
+            break
+            ;;
+        *)
+            echo "Invalid option. Please choose 0-4 or 'exit'."
+            ;;
+    esac
+    echo
+done
